ce59208dc2f91bdde9b36d0f9a7a6fdd4e25db8a
alekseymalloy <93867037+alekseymalloy@users.noreply.github.com>
2022-04-26 23:09:49 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,19 @@ flsFunctions.isWebP();
 
   // // init Swiper:
   // const swiper = new Swiper();
+  // $(document).ready(function () {
+  //   $('select').niceSelect();
+  
+  //   $(".slider-template").slick({
+  //     dots: true,
+  //     slidesToShow: 3,
+  //     prevArrow:"<div class='prevArrow'><img src='img/icon-left.png'></div>",
+  //     nextArrow:"<div class='nextArrow'><img src='img/icon-right.png'></div>",
+  //   })
+  // });
+  
+
+
+  
+  
+  
--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,23 @@ $(document).ready(function () {
     prevArrow:"<div class='prevArrow'><img src='img/icon-left.png'></div>",
     nextArrow:"<div class='nextArrow'><img src='img/icon-right.png'></div>",
   })
+
+
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    appendArrows: ".container-arrows",
+  });
+  $('.multiple-items2').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    appendArrows: ".container-arrows2",
+  });
 });
+
+
 
 
 
